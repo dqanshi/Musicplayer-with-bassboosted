ddc08199cfe1b5a545227d415306dfb3b6a597c4
SOUL々H҉A҉C҉K҉E҉R҉ <63580147+dqanshi@users.noreply.github.com>
2023-05-28 20:26:04 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,150 +1,92 @@
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.media.MediaPlayer;
-import android.net.Uri;
+
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private MediaPlayer mediaPlayer;
-    private List<String> songPaths;
-    private int currentSongIndex;
+
+    private Button buttonPlay;
+    private Button buttonPause;
+    private Button buttonStop;
+    private Button buttonBass;
+    private BassBoost bassBoost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mediaPlayer = new MediaPlayer();
-        songPaths = new ArrayList<>();
-        currentSongIndex = 0;
+        buttonPlay = findViewById(R.id.buttonPlay);
+        buttonPause = findViewById(R.id.buttonPause);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonBass = findViewById(R.id.buttonBass);
 
-        Button playButton = findViewById(R.id.playButton);
-        Button stopButton = findViewById(R.id.stopButton);
-        Button nextButton = findViewById(R.id.nextButton);
-
-        playButton.setOnClickListener(new View.OnClickListener() {
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (songPaths.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "No songs found", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                if (!mediaPlayer.isPlaying()) {
-                    playSong(songPaths.get(currentSongIndex));
-                } else {
-                    mediaPlayer.pause();
-                }
+                // Start playback logic here
+                Toast.makeText(MainActivity.this, "Play button clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
-        stopButton.setOnClickListener(new View.OnClickListener() {
+        buttonPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.pause();
-                }
-                mediaPlayer.seekTo(0);
+                // Pause playback logic here
+                Toast.makeText(MainActivity.this, "Pause button clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
-        nextButton.setOnClickListener(new View.OnClickListener() {
+        buttonStop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (songPaths.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "No songs found", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                currentSongIndex++;
-                if (currentSongIndex >= songPaths.size()) {
-                    currentSongIndex = 0;
-                }
-
-                if (mediaPlayer.isPlaying()) {
-                    mediaPlayer.stop();
-                }
-                playSong(songPaths.get(currentSongIndex));
+                // Stop playback logic here
+                Toast.makeText(MainActivity.this, "Stop button clicked", Toast.LENGTH_SHORT).show();
             }
         });
 
-        checkPermissionsAndReadSongs();
+        buttonBass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggleBassBoost();
+            }
+        });
+
+        // Set background image
+        getWindow().setBackgroundDrawableResource(R.drawable.song_background);
+
+        // Access internal and external storage for songs
+        String internalPath = getFilesDir().getAbsolutePath();
+        String externalPath = getExternalFilesDir(null).getAbsolutePath();
     }
 
-    private void checkPermissionsAndReadSongs() {
-        // Check if the app has permission to access external storage
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
-            // Request the permission if it has not been granted
-            ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                    1);
+    private void toggleBassBoost() {
+        if (bassBoost == null) {
+            BassBoost.Settings settings = new BassBoost.Settings();
+            bassBoost = new BassBoost(0, 0);
+            bassBoost.setProperties(settings);
+            bassBoost.setEnabled(true);
+            Toast.makeText(MainActivity.this, "Bass Boost Enabled", Toast.LENGTH_SHORT).show();
         } else {
-            // Read songs from internal and external storage
-            readSongsFromInternalStorage();
-            readSongsFromExternalStorage();
-        }
-    }
-
-    private void readSongsFromInternalStorage() {
-        File internalStorage = Environment.getExternalStorageDirectory();
-        String internalStoragePath = internalStorage.getAbsolutePath();
-        readSongsFromDirectory(internalStoragePath);
-    }
-
-    private void readSongsFromExternalStorage() {
-        File externalStorage = getExternalFilesDir(null);
-        if (externalStorage != null) {
-            String externalStoragePath = externalStorage.getAbsolutePath();
-            readSongsFromDirectory(externalStoragePath);
-        }
-    }
-
-    private void readSongsFromDirectory(String directoryPath) {
-        File directory = new File(directoryPath);
-        if (directory.exists() && directory.isDirectory()) {
-            File[] files = directory.listFiles();
-            if (files != null) {
-                for (File file : files) {
-                    if (file.isFile() && isAudioFile(file.getName())) {
-                        songPaths.add(file.getAbsolutePath());
-                    }
-                }
+            bassBoost.setEnabled(!bassBoost.getEnabled());
+            if (bassBoost.getEnabled()) {
+                Toast.makeText(MainActivity.this, "Bass Boost Enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Bass Boost Disabled", Toast.LENGTH_SHORT).show();
             }
-        }
-    }
-
-    private boolean isAudioFile(String fileName) {
-        String extension = fileName.substring(fileName.lastIndexOf(".") + 1);
-        return extension.equalsIgnoreCase("mp3") || extension.equalsIgnoreCase("wav");
-    }
-
-    private void playSong(String songPath) {
-        try {
-            mediaPlayer.reset();
-            mediaPlayer.setDataSource(this, Uri.parse(songPath));
-            mediaPlayer.prepare();
-            mediaPlayer.start();
-        } catch (Exception e) {
-            Toast.makeText(this, "Error playing song", Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mediaPlayer.release();
+        if (bassBoost != null) {
+            bassBoost.release();
+            bassBoost = null;
+        }
     }
 }
